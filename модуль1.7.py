2d73fa4be2cbb36242a07facb3b0cd22c23cee5d
Artemglazov <gl.artem10.78@gmail.com>
2024-12-24 15:16:48 +0300

    Add files via upload

---
+++ модуль1.7.py
@@ -0,0 +1,20 @@
+grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+print(grades[0])
+print(grades[1])
+a = sum(grades[0])/len(grades[0])
+b = sum(grades[1])/len(grades[1])
+c = sum(grades[2])/len(grades[2])
+d = sum(grades[3])/len(grades[3])
+e = sum(grades[4])/len(grades[4])
+print(a,b,c,d,e)
+students = list({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'})
+students .sort()
+students[0] = 'Aaron'
+students[1] = 'Bilbo'
+students[2] = 'Johnny'
+students[3] = 'Khendrik'
+students[4] = 'Steve'
+dict_ = {}
+dict_[students[0] ]=[ a] ,[students[1]] =[b] ,[students[2]] = [c],[students[3]] = [d] ,
+[students[4]] =[ e ]
+print(dict_)
